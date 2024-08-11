c39e5ec6b980e8b4142e6890c0b5d2cca91f032e
Edet John <edetjohn213@gmail.com>
2024-08-11 14:26:53 +0100

    still fixing 502 error

--- App.js
+++ App.js
@@ -10,12 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS setup
-app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST'], // Allow only specific methods
-  allowedHeaders: ['Content-Type'], // Allow specific headers
-}));
+// Manual CORS Headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST'); // Allow GET and POST methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // Allow Content-Type header
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
